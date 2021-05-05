a249c99484d724a5f1c704e667932bc452ec5b7a
Leon Fan <leonkind@gmail.com>
2021-05-05 16:07:06 -0500

    not sure what I did

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
+    fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(toy => { 
+      let toyCard = document.querySelector('#toy-collection')
+      let toyName = document.createElement('h2')
+      let toyUrl = document.createElement('img')
+      let toyLikes = document.createElement('p')
+      let toyLikeButton = document.createElement('button')
+      toyCard.appendChild(toyName)
+      toyCard.appendChild(toyUrl)
+      toyCard.appendChild(toyLikes)
+      toyCard.appendChild(toyLikeButton)
+      toyName.innerHTML = toy.name
+      toyUrl.innerHTML = toy.image
+      toyLikes.innerHTML = toy.likes
+    })
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -13,3 +28,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
